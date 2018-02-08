bf02b7611d3a4a5eb90ab7c48e3b4b23e76103be
wb-qjg272639 <wb-qjg272639@alibaba-inc.com>
2018-02-08 17:10:57 +0800

    add web config

---
+++ saturn-web/src/main/java/com/project/tool/web/interceptor/RequestInterceptor.java
@@ -0,0 +1,30 @@
+package com.project.tool.web.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by qjg on 2018/2/8.
+ */
+public class RequestInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o)
+        throws Exception {
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o,
+                           ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+                                Object o, Exception e) throws Exception {
+
+    }
+}
